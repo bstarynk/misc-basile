e62a93d37c2df96a3d11737f69919b377308ff8d
Basile Starynkevitch <basile@starynkevitch.net>
2019-08-30 06:31:23 +0200

    adding old-papers/describing-control-1987/

---
+++ old-papers/describing-control-1987/build-pdf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# generate the git tag 
+gittag=$(git log --format=oneline -1 --abbrev=16 --abbrev-commit -q|cut -d' ' -f1)
+git log --format=oneline -1 --abbrev=16 --abbrev-commit -q | awk '{printf "\\newcommand{\\gitcommit}[0]{%s}\n", $1}' > generated-git-commit.tex
+
+# run LaTeX first time
+pdflatex -halt-on-error  desc-ctrl-1987 < /dev/null
+
+# run bibtex for the bibliography
+bibtex desc-ctrl-1987 < /dev/null
+
+# run LaTeX the second time
+pdflatex -halt-on-error  desc-ctrl-1987 < /dev/null
