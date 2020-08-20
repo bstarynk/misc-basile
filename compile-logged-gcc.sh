af2d911bed0946000019c769ef73701bc40c563b
Basile Starynkevitch <basile@starynkevitch.net>
2020-08-20 07:10:43 +0200

    better temporary executable in compile-logged-gcc.sh

--- compile-logged-gcc.sh
+++ compile-logged-gcc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 export GITID=$(git log -1|awk '/commit/{printf ("%.12s\n", $2); }')
 [ -f logged-gcc ] && mv -v logged-gcc  logged-gcc~
-/usr/bin/g++ -o logged-gcc$$ -Wall -Wextra -rdynamic -O -g -std=gnu++17 -DGITID=\"$GITID\" logged-gcc.cc  -lstdc++ && /bin/mv -v  logged-gcc$$ logged-gcc
+/usr/bin/g++ -o logged-gcc_$$ -Wall -Wextra -rdynamic -O -g -std=gnu++17 -DGITID=\"$GITID\" logged-gcc.cc  -lstdc++ && /bin/mv -v  logged-gcc_$$ logged-gcc
 
 [ -f logged-g++ ] || ln -sv logged-gcc logged-g++
