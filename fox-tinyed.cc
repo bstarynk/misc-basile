d46b8abb80fe47baf201bf2dc27ea4ac8ac9b800
Basile Starynkevitch <basile.starynkevitch@cea.fr>
2022-04-19 15:37:31 +0200

    coded a main, but link fails

--- fox-tinyed.cc
+++ fox-tinyed.cc
@@ -2,25 +2,53 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 //  © Copyright 2022 Basile Starynkevitch (&Jeroen van der Zijp)
 //  some code taken from fox-toolkit.org Adie
-#include <ofstream>
+#include <fstream>
 /// fox-toolkit.org
-#include <FX.h>
+#include <fx.h>
 
 // Editor main window
-class TinyTextWindow : public FXMainWindow {
-  FXDECLARE(TextWindow);
+class TinyTextWindow : public FXMainWindow
+{
+  FXDECLARE(TinyTextWindow);
+  ///
   FXHorizontalFrame   *editorframe;             // Editor frame
   FXText              *editor;                  // Editor text widget
+protected:
 public:
-  TinyTextWindow();
+  TinyTextWindow(FXApp *app);
   virtual ~TinyTextWindow();
+  virtual void create();
 };				// end TinyTextWindow
 
-TinyTextWindow::TinyTextWindow()
-  : FXMainWindow(), editorframe(nullptr), editor(null)
+void
+TinyTextWindow::create()
 {
+  FXMainWindow::create();
+  show(PLACEMENT_SCREEN);
+} // end TinyTextWindow::create()
+
+TinyTextWindow::TinyTextWindow(FXApp* app)
+  : FXMainWindow(app,"tiny-text-fox"), editorframe(nullptr), editor(nullptr)
+{
+  editorframe = //
+    new FXHorizontalFrame(this,LAYOUT_SIDE_TOP|FRAME_NONE //
+                          |LAYOUT_FILL_X|LAYOUT_FILL_Y|PACK_UNIFORM_WIDTH);
+  editor = //
+    new FXText (this);
 } // end TinyTextWindow::TinyTextWindow
 
 TinyTextWindow::~TinyTextWindow()
 {
+  delete editorframe;
+  delete editor;
 } // end TinyTextWindow::~TinyTextWindow
+
+int
+main(int argc, char*argv[])
+{
+  FXApp the_app("fox-tinyed","FOX tinyed (Basile)");
+  the_app.init(argc, argv);
+  new TinyTextWindow(&the_app);
+  the_app.create();
+  return the_app.run();
+} // end main
