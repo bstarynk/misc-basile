b689595b84921814984b7ab9a39b42388bf5b77b
Basile Starynkevitch <basile.starynkevitch@cea.fr>
2022-04-19 16:08:43 +0200

    git ignore fox-tinyed, but the window is very small

--- tinyed-build.sh
+++ tinyed-build.sh
@@ -5,4 +5,4 @@ ASTYLE=astyle
 ASTYLEFLAGS='--verbose --indent=spaces=2  --style=gnu'
 $ASTYLE $ASTYLEFLAGS fox-tinyed.cc
 CXXFLAGS='-Wall  -Wextra -Woverloaded-virtual -Wshadow -O -g'
-$CXX $CXXFLAGS $(pkg-config --cflags fox17) fox-tinyed.cc $(pkg-config --libs fox17)
+$CXX $CXXFLAGS $(pkg-config --cflags fox17) fox-tinyed.cc $(pkg-config --libs fox17) -o fox-tinyed
