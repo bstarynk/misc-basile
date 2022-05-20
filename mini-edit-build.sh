a093a8356c71204efa67bf4347b615afca9cd5ef
Basile Starynkevitch <basile.starynkevitch@cea.fr>
2022-05-20 15:51:21 +0200

    compile with -O0 -g for ease of debugging

--- mini-edit-build.sh
+++ mini-edit-build.sh
@@ -4,7 +4,7 @@ ASTYLE=astyle
 FLTKCONFIG=/usr/local/bin/fltk-config
 ASTYLEFLAGS='--verbose --indent=spaces=2  --style=gnu'
 $ASTYLE $ASTYLEFLAGS fltk-mini-edit.cc
-CXXFLAGS='-Wall  -Wextra -Woverloaded-virtual -Wshadow -O -g -std=gnu++17'
+CXXFLAGS='-Wall  -Wextra -Woverloaded-virtual -Wshadow -O0 -g -std=gnu++17'
 GITID=$(git log --format=oneline -q -1 | cut '-d '  -f1 | tr -d '\n' | head -16c)
 $CXX $CXXFLAGS $($FLTKCONFIG --cflags) -DGITID=\"$GITID\" fltk-mini-edit.cc \
      $($FLTKCONFIG --libs) -lunistring -lX11 -lXext -lXinerama \
