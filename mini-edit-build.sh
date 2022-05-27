621680d4155b9cc78b5387fbec349696a1f84eab
Basile Starynkevitch <basile@starynkevitch.net>
2022-05-27 13:07:21 +0200

    backup on fltk-mini-edit executable

--- mini-edit-build.sh
+++ mini-edit-build.sh
@@ -6,6 +6,9 @@ ASTYLEFLAGS='--verbose --indent=spaces=2  --style=gnu'
 $ASTYLE $ASTYLEFLAGS fltk-mini-edit.cc
 CXXFLAGS='-Wall  -Wextra -Woverloaded-virtual -Wshadow -O0 -g -std=gnu++17'
 GITID=$(git log --format=oneline -q -1 | cut '-d '  -f1 | tr -d '\n' | head -16c)
+if [ -x fltk-mini-edit ]; then
+    /bin/mv -v fltk-mini-edit fltk-mini-edit~
+fi
 $CXX $CXXFLAGS $($FLTKCONFIG --cflags) -DGITID=\"$GITID\" \
      $(pkg-config --cflags jsoncpp) \
      fltk-mini-edit.cc \
