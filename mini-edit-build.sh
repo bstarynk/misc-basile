cf7238391d668533bc9754b4d97fb3386aaea26a
Basile Starynkevitch <basile@starynkevitch.net>
2022-05-27 09:05:15 +0200

    use JSONCPP

--- mini-edit-build.sh
+++ mini-edit-build.sh
@@ -6,9 +6,13 @@ ASTYLEFLAGS='--verbose --indent=spaces=2  --style=gnu'
 $ASTYLE $ASTYLEFLAGS fltk-mini-edit.cc
 CXXFLAGS='-Wall  -Wextra -Woverloaded-virtual -Wshadow -O0 -g -std=gnu++17'
 GITID=$(git log --format=oneline -q -1 | cut '-d '  -f1 | tr -d '\n' | head -16c)
-$CXX $CXXFLAGS $($FLTKCONFIG --cflags) -DGITID=\"$GITID\" fltk-mini-edit.cc \
+$CXX $CXXFLAGS $($FLTKCONFIG --cflags) -DGITID=\"$GITID\" \
+     $(pkg-config --cflags jsoncpp) \
+     fltk-mini-edit.cc \
      -L/usr/local/lib \
-     $($FLTKCONFIG --libs) -lunistring -lX11 -lXext -lXinerama \
+     $($FLTKCONFIG --libs) \
+     $(pkg-config --libs jsoncpp) \
+     -lunistring -lX11 -lXext -lXinerama \
      -lXcursor -lXrender -lXrandr -lXfixes -lXi -lfreetype \
      -lfontconfig -lXft -lGL -ldl -lpthread -lrt -ljpeg -lpng -ltiff -lz -lGL \
      -lbacktrace \
