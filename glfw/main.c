1ede9d323c174364f8b721674a2d25dade6186d4
Abhishek Chakravarti <abhishek@taranjali.org>
2022-06-04 10:17:16 +0530

    Create empty GLFW window
    GLFW is a toolkit like SDL that manages the creation of an OpenGL
    context within a window, and also handles the common events. GLFW seems
    to be less functional than SDL, but should be adequate for our purpose
    now.
    
    We are now drawing an empty window, which will be the template for
    further development. Our next step is to be able to create the OpenGL
    context and draw a triangle in it. Subsequently, we will attempt to
    incorporate the ImGUI toolkit.
    
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

---
+++ glfw/main.c
@@ -0,0 +1,63 @@
+#include "include/glad/glad.h"
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void process_input(GLFWwindow *window);
+
+int main(void)
+{
+        glfwInit();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+        GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "RefPerSys", NULL, NULL);
+        if (!window) {
+                printf("Failed to create GLFW window\n");
+                glfwTerminate();
+                return -1;
+        }
+
+        glfwMakeContextCurrent(window);
+        glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+        if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+                printf("Failed to initialise GLAD\n");
+                glfwTerminate();
+                return -1;
+        }
+
+        while (!glfwWindowShouldClose(window)) {
+                process_input(window);
+                glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+                glClear(GL_COLOR_BUFFER_BIT);
+                glfwSwapBuffers(window);
+                glfwPollEvents();
+        }
+
+        glfwTerminate();
+        return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+        glViewport(0, 0, width, height);
+}
+
+
+void process_input(GLFWwindow *window)
+{
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+                glfwSetWindowShouldClose(window, true);
+}
