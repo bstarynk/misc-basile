0ecceb56554959ae06f7a825d50e9c6b7874ccaa
Basile Starynkevitch <basile@starynkevitch.net>
2022-11-30 19:18:58 +0100

    better manydl.c for computation of CPU time in deferreed

--- manydl.c
+++ manydl.c
@@ -332,26 +332,30 @@ waitdeferred (int rank, double tstart, pid_t pid, int deferredl,
   int waitcnt = 0;
   int wstat = 0;
   pid_t wpid = 0;
-  struct rusage ruswself = { };
+  struct rusage ruswchilstart = { };
   struct rusage ruswchil = { };
   fflush (NULL);
+  getrusage (RUSAGE_CHILDREN, &ruswchilstart);
   // if the previous compilation is still running, wait for it.
   do
     {
-      memset (&ruswself, 0, sizeof (ruswself));
       memset (&ruswchil, 0, sizeof (ruswchil));
-      wpid = wait4 (pid, &wstat, WEXITED, &ruswself);
+      wpid = waitpid (pid, &wstat, WEXITED);
       getrusage (RUSAGE_CHILDREN, &ruswchil);
       waitcnt++;
     }
   while (wstat == 0 && wpid != pid && waitcnt < 4);
   double tim_end = my_clock (CLOCK_MONOTONIC);
   double usertime =
-    1.0 * ruswself.ru_utime.tv_sec + 1.0e-6 * ruswself.ru_utime.tv_usec
-    + 1.0 * ruswchil.ru_utime.tv_sec + 1.0e-6 * ruswchil.ru_utime.tv_usec;
+    1.0 * ruswchil.ru_utime.tv_sec + 1.0e-6 * ruswchil.ru_utime.tv_usec
+    -
+    1.0 * ruswchilstart.ru_utime.tv_sec +
+    1.0e-6 * ruswchilstart.ru_utime.tv_usec;
   double systime =
-    1.0 * ruswself.ru_utime.tv_sec + 1.0e-6 * ruswself.ru_utime.tv_usec
-    + 1.0 * ruswchil.ru_stime.tv_sec + 1.0e-6 * ruswchil.ru_stime.tv_usec;
+    1.0 * ruswchil.ru_stime.tv_sec + 1.0e-6 * ruswchil.ru_stime.tv_usec
+    -
+    1.0 * ruswchilstart.ru_utime.tv_sec +
+    1.0e-6 * ruswchilstart.ru_utime.tv_usec;
   if (timedataf)
     fprintf (timedataf,
 	     "NM='%s' IX=%d SZ=%d ELAPSED=%.3f UCPU=%.4f SCPU=%.4f\n",
