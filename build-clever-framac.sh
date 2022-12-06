42059a1e9dcd7307f6f38c44a476098c7374458f
Basile Starynkevitch <basile@starynkevitch.net>
2022-12-06 11:26:24 +0100

    build-clever-framac.sh indenting with astyle clever-framac.cc

--- build-clever-framac.sh
+++ build-clever-framac.sh
@@ -1,2 +1,9 @@
 #!/bin/bash -x
+# SPDX-License-Identifier: GPL-3.0-or-later
 # file misc-basile/build-clever-framac.sh
+# Author(s):
+#     Basile Starynkevitch <basile@starynkevitch.net>
+#
+# © Copyright 2022 Commissariat à l'Energie Atomique
+/usr/bin/astyle -v --style=gnu clever-framac.cc
+/usr/bin/g++ -O -g -rdynamic clever-framac.cc -ldl -o clever-framac
