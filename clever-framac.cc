1a92abaa89f4cfc5292a174754b54ff4834090a6
Basile Starynkevitch <basile@starynkevitch.net>
2022-12-06 11:29:23 +0100

    uncomplete clever-framac.cc

--- clever-framac.cc
+++ clever-framac.cc
@@ -26,10 +26,40 @@
 #include <fstream>
 #include <iostream>
 #include <unistd.h>
+#include <getopt.h>
+
+
+const char*progname;
+int verbose_flag;
+int version_flag;
+void
+parse_program_arguments(int argc, char**argv)
+{
+    int c=0;
+    int option_index= -1;
+#warning incomplete parse_program_arguments in clever-framac.cc
+    static const struct option long_clever_options[] =
+    {
+        {"verbose", no_argument, &verbose_flag, 'V'},
+        {0,0,0,0}
+    };
+    do
+        {
+            option_index = -1;
+            c = getopt_long(argc, argv,
+                            "V",
+                            long_clever_options, &option_index);
+            if (c<0)
+                break;
+        }
+    while (c>=0);
+} // end parse_program_arguments
 
 int
 main(int argc, char*argv[])
 {
+    progname = argv[0];
+    parse_program_arguments(argc, argv);
 } // end main
 
 /****************
