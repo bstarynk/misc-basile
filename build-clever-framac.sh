1d21513398d382fb01676e847581e7e2eda5a333
Basile Starynkevitch <basile@starynkevitch.net>
2022-12-06 14:23:26 +0100

    build-clever-framac.sh set GIT_ID macro

--- build-clever-framac.sh
+++ build-clever-framac.sh
@@ -5,5 +5,6 @@
 #     Basile Starynkevitch <basile@starynkevitch.net>
 #
 # © Copyright 2022 Commissariat à l'Energie Atomique
+GITID=$(git log --format=oneline -q -1 | cut '-d '  -f1 | tr -d '\n' | head -12c)
 /usr/bin/astyle -v --style=gnu clever-framac.cc
-/usr/bin/g++ -O -g -rdynamic clever-framac.cc -ldl -o clever-framac
+/usr/bin/g++ -O -g -rdynamic clever-framac.cc -DGIT_ID=\"$GITID\" -ldl -o clever-framac
