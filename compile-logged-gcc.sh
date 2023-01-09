6fc3ce449524cc56960a7fe93a4ba1a579042946
Basile Starynkevitch <basile@starynkevitch.net>
2023-01-09 14:39:39 +0100

    compile-logged-gcc.sh link with -L /usr/local/lib/

--- compile-logged-gcc.sh
+++ compile-logged-gcc.sh
@@ -7,6 +7,7 @@ export GITID=$(git log -1|awk '/commit/{printf ("%.12s\n", $2); }')
 export MYPACKAGES='openssl sqlite3'
 [ -f logged-gcc ] && mv -v logged-gcc  logged-gcc~
 /usr/bin/g++ -o logged-gcc_$$ -Wall -Wextra -rdynamic \
+	     -L /usr/local/lib/ \
 	     $(pkg-config --cflags $MYPACKAGES) \
 	     -O2 -g -std=gnu++17 -DGITID=\"$GITID\" \
 	     logged-gcc.cc \
