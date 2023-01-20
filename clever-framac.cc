06466ab6140a17ffb560e3dcf8df95cd2a68eadc
Basile Starynkevitch <basile@starynkevitch.net>
2023-01-20 16:15:01 +0100

    reorganized clever-framac.cc

--- clever-framac.cc
+++ clever-framac.cc
@@ -92,101 +92,17 @@ public:
 std::map<std::string, Source_file*> Source_file::srcf_dict;
 std::vector<Source_file> my_srcfiles;
 
-void
-add_source_file(const char*srcpath)
-{
-    struct stat srcstat = {};
-    assert(srcpath != nullptr);
-    if (stat(srcpath, &srcstat) < 0)
-        {
-            fprintf(stderr, "%s: failed to stat %s on %s (%s)\n",
-                    progname, srcpath, myhost, strerror(errno));
-            exit(EXIT_FAILURE);
-        };
-    if (srcstat.st_mode & S_IFMT != S_IFREG)
-        {
-            fprintf(stderr, "%s: source %s is not a regular file on %s\n",
-                    progname, srcpath, myhost);
-            exit(EXIT_FAILURE);
-        };
-    const char* rp = realpath(srcpath, nullptr);
-    if (!rp || !rp[0])
-        {
-            fprintf(stderr, "%s: failed to find real path of %s on %s (%s)\n",
-                    progname, srcpath, myhost, strerror(errno));
-            exit(EXIT_FAILURE);
-        };
-    int lenrp = strlen(rp);
-    if (lenrp < 4)
-        {
-            fprintf(stderr, "%s: real path of %s on %s is too short: %s\n",
-                    progname, srcpath, myhost, rp);
-            exit(EXIT_FAILURE);
-        };
-    if (rp[lenrp-1] == 'c' && rp[lenrp-2] == '.')
-        {
-            /* add a C file */
-            Source_file csrc(rp, srcty_c);
-            my_srcfiles.push_back(csrc);
-        }
-    else if (rp[lenrp-3] == '.'
-             && rp[lenrp-2] == 'c' && rp[lenrp-1] == 'c')
-        {
-            /* add a C++ file */
-            Source_file cppsrc(rp, srcty_cpp);
-            my_srcfiles.push_back(cppsrc);
-        }
-    free((void*)rp);
-} // end add_source_file
 
-void
-add_sources_list(const char*listpath)
-{
-    char* linbuf = nullptr;
-    size_t sizbuf = 0;
-    assert(listpath != nullptr);
-    bool isapipe = listpath[0] == '!' || listpath[0] == '|';
-    FILE *f = isapipe?popen(listpath+1, "r"):fopen(listpath, "r");
-    if (!f)
-        {
-            fprintf(stderr, "%s: failed to add sources using %s %s (%s)\n",
-                    progname, isapipe?"pipe":"file",
-                    listpath, strerror(errno));
-            exit(EXIT_FAILURE);
-        };
-    const int inisiz=128;
-    linbuf = (char*)calloc(1, inisiz);
-    if (!linbuf)
-        {
-            fprintf(stderr, "%d: failed to allocate line buffer (near %s:%d) of %d bytes (%s)\n",
-                    progname, __FILE__, __LINE__, inisiz, strerror(errno));
-            exit(EXIT_FAILURE);
-        }
-    sizbuf = (size_t)inisiz;
-    do
-        {
-            memset (linbuf, 0, sizbuf);
-            ssize_t rsz = getline(&linbuf, &sizbuf, f);
-            if (rsz<0)
-                break;
-            if (linbuf==nullptr)
-                break;
-            if (rsz < sizbuf && linbuf[rsz] == '\n')
-                linbuf[rsz] = (char)0;
-            if (linbuf[0] == '#')
-                continue;
-            if (linbuf[0] == '@')
-                add_sources_list(linbuf+1);
-            else
-                add_source_file(linbuf);
-        }
-    while (!feof(f));
-    free(linbuf);
-    if (isapipe)
-        pclose(f);
-    else
-        fclose(f);
-} // end listpath
+/// add a source file to analyze
+void add_source_file(const char*srcpath);
+
+
+/// Add a file containing the list of files to analyze;  empty lines
+/// and lines starting with an hash # are skipped, per Unix tradition.
+/// a line starting with @ means that it is a list of files
+/// a line starting with | or ! means to popen to get the list of files.
+void add_sources_list(const char*listpath);
+
 
 enum clever_flags_en
 {
@@ -220,8 +136,9 @@ show_help(void)
            "\t -I <incldir>           # preprocessing include\n"
            "\t -D <definition>        # preprocessing definition\n"
            "\t -U <undefine>          # preprocessing undefine\n"
-           "\t -l | --sources <slist> # read list of files from <sfile>\n"
+           "\t -l | --sources <slist> # read list of files (one per line) from <sfile>\n"
            "\t                        # if it starts with ! or | use popen\n"
+           "\t                        # if it starts with @ it a list of files\n"
            "\t ... <source files>     # sources are files *.c ...\n"
            "\t                        # ... or C++ files *.cc\n"
            " (preprocessing options are passed to Frama-C)\n"
@@ -365,6 +282,104 @@ parse_program_arguments(int argc, char**argv)
 } // end parse_program_arguments
 
 
+void
+add_source_file(const char*srcpath)
+{
+    struct stat srcstat = {};
+    assert(srcpath != nullptr);
+    if (stat(srcpath, &srcstat) < 0)
+        {
+            fprintf(stderr, "%s: failed to stat %s on %s (%s)\n",
+                    progname, srcpath, myhost, strerror(errno));
+            exit(EXIT_FAILURE);
+        };
+    if (srcstat.st_mode & S_IFMT != S_IFREG)
+        {
+            fprintf(stderr, "%s: source %s is not a regular file on %s\n",
+                    progname, srcpath, myhost);
+            exit(EXIT_FAILURE);
+        };
+    const char* rp = realpath(srcpath, nullptr);
+    if (!rp || !rp[0])
+        {
+            fprintf(stderr, "%s: failed to find real path of %s on %s (%s)\n",
+                    progname, srcpath, myhost, strerror(errno));
+            exit(EXIT_FAILURE);
+        };
+    int lenrp = strlen(rp);
+    if (lenrp < 4)
+        {
+            fprintf(stderr, "%s: real path of %s on %s is too short: %s\n",
+                    progname, srcpath, myhost, rp);
+            exit(EXIT_FAILURE);
+        };
+    if (rp[lenrp-1] == 'c' && rp[lenrp-2] == '.')
+        {
+            /* add a C file */
+            Source_file csrc(rp, srcty_c);
+            my_srcfiles.push_back(csrc);
+        }
+    else if (rp[lenrp-3] == '.'
+             && rp[lenrp-2] == 'c' && rp[lenrp-1] == 'c')
+        {
+            /* add a C++ file */
+            Source_file cppsrc(rp, srcty_cpp);
+            my_srcfiles.push_back(cppsrc);
+        }
+    free((void*)rp);
+} // end add_source_file
+
+void
+add_sources_list(const char*listpath)
+{
+    char* linbuf = nullptr;
+    size_t sizbuf = 0;
+    assert(listpath != nullptr);
+    bool isapipe = listpath[0] == '!' || listpath[0] == '|';
+    FILE *f = isapipe?popen(listpath+1, "r"):fopen(listpath, "r");
+    if (!f)
+        {
+            fprintf(stderr, "%s: failed to add sources using %s %s (%s)\n",
+                    progname, isapipe?"pipe":"file",
+                    listpath, strerror(errno));
+            exit(EXIT_FAILURE);
+        };
+    const int inisiz=128;
+    linbuf = (char*)calloc(1, inisiz);
+    if (!linbuf)
+        {
+            fprintf(stderr, "%d: failed to allocate line buffer (near %s:%d) of %d bytes (%s)\n",
+                    progname, __FILE__, __LINE__, inisiz, strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+    sizbuf = (size_t)inisiz;
+    do
+        {
+            memset (linbuf, 0, sizbuf);
+            ssize_t rsz = getline(&linbuf, &sizbuf, f);
+            if (rsz<0)
+                break;
+            if (linbuf==nullptr)
+                break;
+            if (rsz < sizbuf && linbuf[rsz] == '\n')
+                linbuf[rsz] = (char)0;
+            if (linbuf[0] == '#')
+                continue;
+            if (linbuf[0] == '@')
+                add_sources_list(linbuf+1);
+            else
+                add_source_file(linbuf);
+        }
+    while (!feof(f));
+    free(linbuf);
+    if (isapipe)
+        pclose(f);
+    else
+        fclose(f);
+} // end add_sources_list
+
+
+
 int
 main(int argc, char*argv[])
 {
@@ -373,8 +388,8 @@ main(int argc, char*argv[])
         verbose_opt = true;
     parse_program_arguments(argc, argv);
     if (verbose_opt)
-        printf("%s running verbosely on %s pid %d git %s\n", progname,
-               myhost, (int)getpid(), GIT_ID);
+        printf("%s running verbosely on %s pid %d git %s with %d source files\n", progname,
+               myhost, (int)getpid(), GIT_ID, (int) my_srcfiles.size());
 #warning should run framac on the collected source files....
 } // end main
 
