0a2f03e6fee033a074a728e2e866b7445ac0e32e
Basile Starynkevitch <basile@starynkevitch.net>
2023-03-27 15:33:14 +0200

    dont compile for destruction of union

--- CarburEx/carbex.hh
+++ CarburEx/carbex.hh
@@ -14,4 +14,32 @@
 
 extern "C" const char*carbex_progname;
 
+enum TokType
+{
+  tky_none,
+  tky_int,
+  tky_double,
+  tky_string,
+  //  tky_name,
+};
+class Tok
+{
+  enum TokType tk_type;
+  union
+  {
+    int tk_int;
+    double tk_double;
+    std::string tk_string;
+  };
+protected:
+  Tok(TokType ty, int n): tk_type(ty), tk_int(n) {};
+  Tok(TokType ty, double d): tk_type(ty), tk_double(d) {};
+  Tok(TokType ty, std::string s): tk_type(ty), tk_string(s) {};
+public:
+  virtual ~Tok();
+  Tok(int n) : Tok(tky_int, n) {};
+  Tok(double d) : Tok(tky_double, d) {};
+  Tok(std::string s): Tok(tky_string, s) {};
+};
+
 #endif /*CARBEX_INCLUDED*/
--- CarburEx/main.cc
+++ CarburEx/main.cc
@@ -4,18 +4,36 @@
 
 const char*carbex_progname;
 
+Tok::~Tok()
+{
+  switch (tk_type)
+    {
+    case tky_int:
+      tk_int=0;
+      break;
+    case tky_double:
+      tk_double=0.0;
+      break;
+    case tky_string:
+      tk_string.empty();
+      break;
+    };
+} // end destructor of Tok
+
 void
-show_version(void) {
+show_version(void)
+{
   std::cout << carbex_progname
-	    << " version " << GITID
-	    << " built " << __DATE__ "@" __TIME__ << std::endl;
+            << " version " << GITID
+            << " built " << __DATE__ "@" __TIME__ << std::endl;
 } // end show_version
 
 void
-show_help(void) {
+show_help(void)
+{
   std::cout << carbex_progname << "usage:" << std::endl;
   std::cout << "\t --version # show version" << std::endl;
-    std::cout << "\t --help # show this help" << std::endl;
+  std::cout << "\t --help # show this help" << std::endl;
 } // end show_help
 
 int
