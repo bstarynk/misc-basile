6057630f468f88960443b1db3d08dc6ba8be2f26
Basile Starynkevitch <basile@starynkevitch.net>
2023-03-27 11:07:05 +0200

    add GNUmakefile and carbex.hh

---
+++ CarburEx/carbex.hh
@@ -0,0 +1,13 @@
+// file misc-basile/CarburEx/carbex.hh
+// by Basile Starynkevitch 2023
+#ifndef CARBEX_INCLUDED
+#define CARBEX_INCLUDED
+
+#include <unistd.h>
+#include <time.h>
+
+#include <map>
+#include <vector>
+
+
+#endif /*CARBEX_INCLUDED*/
