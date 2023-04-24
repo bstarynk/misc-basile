46a529068fa037af26f8df3591e2e44bf89868d2
Basile Starynkevitch <basile@starynkevitch.net>
2023-04-24 11:25:00 +0200

    adding CryptArithm/README.mk CryptArithm/neuf+deux=onze/README.mk

---
+++ CryptArithm/README.mk
@@ -0,0 +1,8 @@
+# Cryptarithmetic puzzles
+
+This `misc-basile/CryptArithm` directory in https://github.com/bstarynk/misc-basile/ has some cryptarithmetic puzzles. See https://en.wikipedia.org/wiki/Verbal_arithmetic
+
+## cryptadditions
+
+In directory `neuf+deux=onze` the puzzle NEUF+DEUX=ONZE
+(in French,  `neuf` is 9 (nine), `deux` is 2 (two), and `onze` is 11 (eleven)...)
---
+++ CryptArithm/neuf+deux=onze/README.mk
@@ -0,0 +1,3 @@
+## description
+
+Solve `NEUF` + `DEUX` = `ONZE`
