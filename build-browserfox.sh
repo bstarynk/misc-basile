69c1be556a946b270550e8411c66d508fc237b08
Basile Starynkevitch <basile@starynkevitch.net>
2023-04-25 15:46:40 +0200

    should compile libfox 1.7

--- build-browserfox.sh
+++ build-browserfox.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 # SPDX-License-Identifier: GPL-3.0-or-later
 # file misc-basile/build-browserfox.sh
-# © 2022 copyright CEA & Basile Starynkevitch
+# © 2022 - 2023 copyright CEA & Basile Starynkevitch
 CXX=g++
 ASTYLE=astyle
 ASTYLEFLAGS='--verbose --indent=spaces=2  --style=gnu'
 $ASTYLE $ASTYLEFLAGS browserfox.cc
-CXXFLAGS='-Wall  -Wextra -Woverloaded-virtual -Wshadow -Og -g'
+CXXFLAGS='-Wall  -Wextra -Woverloaded-virtual -Wshadow -O1 -g'
 $CXX $CXXFLAGS $(pkg-config --cflags fox17) browserfox.cc $(pkg-config --libs fox17) \
     -o browserfox
