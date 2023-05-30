7874caae65e2dbe7d6106aa41142339c0e2b124b
Basile Starynkevitch <basile@starynkevitch.net>
2023-05-30 15:38:35 +0200

    browserfox.cc compiles

--- browserfox.cc
+++ browserfox.cc
@@ -3,18 +3,18 @@
 // © 2022 - 2023 copyright CEA & Basile Starynkevitch
 
 /****
-* This program is free software: you can redistribute it and/or modify          *
-* it under the terms of the GNU General Public License as published by          *
-* the Free Software Foundation, either version 3 of the License, or             *
-* (at your option) any later version.                                           *
-*                                                                               *
-* This program is distributed in the hope that it will be useful,               *
-* but WITHOUT ANY WARRANTY; without even the implied warranty of                *
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                 *
-* GNU General Public License for more details.                                  *
-*                                                                               *
-* You should have received a copy of the GNU General Public License             *
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.         *
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ****/
 
 #include <unistd.h>
@@ -38,10 +38,47 @@ const char my_buildtime[]=__DATE__ "@" __TIME__;
 
 const char*progname;
 
-int main(int argc, const char**argv)
+
+class BfWindow: public FXMainWindow
 {
-    progname = argv[0];
-    FXApp application("browserfox");
+  FXDECLARE(BfWindow)
+private:
+protected:
+  BfWindow() {};
+public:
+  BfWindow(FXApp*app);
+  virtual ~BfWindow();
+  virtual void create();
+};				// end class BfWindow
+FXDEFMAP(BfWindow) BfWindowMap[]=
+{
+};
+
+
+
+////////////////////////////////////////////////////////////////
+BfWindow::BfWindow(FXApp*app)
+  : FXMainWindow(app, "browserfox")
+{
+}; // end BfWindow::BfWindow
+
+BfWindow::~BfWindow()
+{
+}; // end BfWindow::~BfWindow
+
+void
+BfWindow::create()
+{
+}; // end BfWindow::create
+
+FXIMPLEMENT(BfWindow,FXMainWindow,BfWindowMap,ARRAYNUMBER(BfWindowMap));
+
+int main(int argc, char**argv)
+{
+  progname = argv[0];
+  FXApp application("browserfox");
+  application.init(argc, argv);
+  BfWindow win(&application);
 } // end main
 
 /****************
