9eaaccbf8017181b1f4a6e6318d0d622f0231f21
Basile Starynkevitch <basile@starynkevitch.net>
2024-02-12 19:45:08 +0100

    build-clever-framac.sh dont indent

--- build-clever-framac.sh
+++ build-clever-framac.sh
@@ -5,10 +5,11 @@
 #     Basile Starynkevitch <basile@starynkevitch.net>
 #
 # © Copyright 2022 - 2023 Commissariat à l'Energie Atomique
+# © Copyright 2024 Basile Starynkevitch
 GITID=$(git log --format=oneline -q -1 | cut '-d '  -f1 | tr -d '\n' | head -12c)
-/usr/bin/astyle -v --style=gnu clever-framac.cc
 /usr/bin/g++ -O -g $(guile-config compile) -rdynamic \
 	     -DGIT_ID=\"$GITID\"  \
 	     -DFULL_CLEVER_FRAMAC_SOURCE=\"$(realpath clever-framac.cc)\" \
 	     clever-framac.cc -ldl \
 	     $(guile-config link) -o clever-framac
+#/usr/bin/astyle -v --style=gnu clever-framac.cc
