7c2bfad28002e658da319c65ae794be4cdbbbada
Basile Starynkevitch <basile@starynkevitch.net>
2024-02-19 12:01:09 +0100

    fltk-mini-edit is linked correctly

--- mini-edit-build.sh
+++ mini-edit-build.sh
@@ -1,19 +1,20 @@
 #!/bin/bash -x
 CXX=g++
 #ASTYLE=astyle
-FLTKCONFIG=/usr/local/bin/fltk-config
+FLTKCONFIG=fltk-config
 #ASTYLEFLAGS='--verbose --indent=spaces=2  --style=gnu'
 #$ASTYLE $ASTYLEFLAGS fltk-mini-edit.cc
 CXXFLAGS='-Wall  -Wextra -Woverloaded-virtual -Wshadow -O1 -g -std=gnu++17'
 CXX_SOURCE_DIR=$(/bin/pwd)
+MINIED_PACKAGES="xrandr xrender jsoncpp xinerama xcursor xft pangoxft fontconfig xdamage xext xextproto xcomposite xfixes"
 GITID=$(git log --format=oneline -q -1 | cut '-d '  -f1 | tr -d '\n' | head -16c)
 $CXX $CXXFLAGS $($FLTKCONFIG --cflags) \
      -DGITID=\"$GITID\" \
      -DCXX_SOURCE_DIR=\"$CXX_SOURCE_DIR\" \
      -DCOMPILE_SCRIPT=\"$0\" \
-     $(pkg-config --cflags jsoncpp) \
+     $(pkg-config --cflags $MINIED_PACKAGES) \
      fltk-mini-edit.cc \
      $($FLTKCONFIG --libs) \
-     $(pkg-config --libs jsoncpp) \
-     -lbacktrace -lunistring \
+     $(pkg-config --libs $MINIED_PACKAGES) \
+     -lbacktrace -lunistring  \
    -o fltk-mini-edit
