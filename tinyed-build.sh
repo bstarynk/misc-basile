cab487cac9cc9f21c1b35be6eddad75d3b3a7041
Basile Starynkevitch <basile@starynkevitch.net>
2024-02-19 08:57:28 +0100

    dont astyle in tinyed-build.sh

--- tinyed-build.sh
+++ tinyed-build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -x
 # file misc-basile/tinyed-build.sh
 CXX=g++-13
-ASTYLE=astyle
-ASTYLEFLAGS='--verbose --indent=spaces=2  --style=gnu'
-$ASTYLE $ASTYLEFLAGS fox-tinyed.cc
+#ASTYLE=astyle
+#ASTYLEFLAGS='--verbose --indent=spaces=2  --style=gnu'
+#$ASTYLE $ASTYLEFLAGS fox-tinyed.cc
+GIT_ID=$(git log --format=oneline -q -1 | cut -c1-10)
 CXXFLAGS='-Wall  -Wextra -Woverloaded-virtual -Wshadow -O -g'
-$CXX $CXXFLAGS $(pkg-config --cflags fox17) fox-tinyed.cc $(pkg-config --libs fox17) -o fox-tinyed
+$CXX $CXXFLAGS  -DGIT_ID=\"$GIT_ID\" $(pkg-config --cflags fox17) fox-tinyed.cc $(pkg-config --libs fox17) -o fox-tinyed
