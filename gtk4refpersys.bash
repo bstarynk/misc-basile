5a74797f13893215109d2fb57f86449c479f4666
Basile Starynkevitch <basile@starynkevitch.net>
2024-02-20 07:49:42 +0100

    adding gtk4refpersys.bash & gtk4refpersys.ui

---
+++ gtk4refpersys.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+## SPDX-License-Identifier: GPL-3.0-or-later
+##  © Copyright 2024 by Basile Starynkevitch
+## program released under GNU General Public License v3+
+##
+## this is free software; you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free
+## Software Foundation; either version 3, or (at your option) any later
+## version.
+##
+## this is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+## or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+## License for more details.
+/usr/bin/make gtk4serv
