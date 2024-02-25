d913c69b27a760c86f3d3cd83d2be37841037f50
Basile Starynkevitch <basile@starynkevitch.net>
2024-02-25 07:56:59 +0100

    longer GIT_ID

--- tinyed-build.sh
+++ tinyed-build.sh
@@ -4,6 +4,6 @@ CXX=g++-13
 #ASTYLE=astyle
 #ASTYLEFLAGS='--verbose --indent=spaces=2  --style=gnu'
 #$ASTYLE $ASTYLEFLAGS fox-tinyed.cc
-GIT_ID=$(git log --format=oneline -q -1 | cut -c1-10)
+GIT_ID=$(git log --format=oneline -q -1 | cut -c1-14)
 CXXFLAGS='-Wall  -Wextra -Woverloaded-virtual -Wshadow -O -g'
 $CXX $CXXFLAGS  -DGIT_ID=\"$GIT_ID\" $(pkg-config --cflags fox17) fox-tinyed.cc $(pkg-config --libs fox17) -o fox-tinyed
