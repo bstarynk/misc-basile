015899bbe86e4db5f5a81ddf1c299997d891d6ee
Basile Starynkevitch <basile@starynkevitch.net>
2024-05-27 11:09:29 +0200

    adding set_process_limits
    Signed-off-by: Basile Starynkevitch <basile@starynkevitch.net>

--- onionrefpersys.c
+++ onionrefpersys.c
@@ -30,6 +30,8 @@
 #include <stdatomic.h>
 #include <stdio.h>
 #include <syslog.h>
+#include <sys/types.h>
+#include <sys/resource.h>
 #include <gnu/libc-version.h>
 #include <sqlite3.h>
 
@@ -48,7 +50,9 @@ bool debug = false;
 const char *web_port = "8080";
 const char *web_host = "localhost";
 const char *my_sqlite_path = "/var/tmp/onionrefpersys.sqlite";
-sqlite3*my_sqlite_db;
+sqlite3 *my_sqlite_db;
+int my_cputime_limit = 7200 /* seconds, so two hours */ ;
+int my_filesize_limit_mb = 1024 /* megabytes, so one gigabyte */ ;
 
 const struct option options_arr[] = {
   {.name = (const char *) "debug",	//
@@ -71,6 +75,14 @@ const struct option options_arr[] = {
    .has_arg = required_argument,	//
    .flag = (int *) NULL,	//
    .val = 'H'},			//
+  {.name = (const char *) "cpu-limit",	//
+   .has_arg = required_argument,	//
+   .flag = (int *) NULL,	//
+   .val = 'C'},			//
+  {.name = (const char *) "file-limit",	//
+   .has_arg = required_argument,	//
+   .flag = (int *) NULL,	//
+   .val = 'F'},			//
   {.name = (const char *) "sqlite-base",	//
    .has_arg = required_argument,	//
    .flag = (int *) NULL,	//
@@ -114,7 +126,8 @@ parse_options (int argc, char **argv)
     {
       int option_index = 0;
       int c = 0;
-      c = getopt_long (argc, argv, "DP:H:B:hV", options_arr, &option_index);
+      c =
+	getopt_long (argc, argv, "DP:H:B:C:F:hV", options_arr, &option_index);
       if (c < 0)
 	break;
       switch (c)
@@ -122,25 +135,35 @@ parse_options (int argc, char **argv)
 	case 'D':		// --debug
 	  debug = true;
 	  break;
-	case 'P':
+	case 'P':		// --port <webport>
 	  web_port = optarg;
 	  break;
-	case 'h':
+	case 'h':		// --help
 	  show_usage ();
 	  break;
-	case 'V':
+	case 'V':		// --version
 	  printf ("%s version git %s built %s\n",
 		  progname, gitid, __DATE__ "@" __TIME__);
-	  printf ("sqlite version %s sourceid %s\n", sqlite3_libversion(), sqlite3_sourceid());
-	  printf ("onion version %s\n", onion_version());
+	  printf ("sqlite version %s sourceid %s\n", sqlite3_libversion (),
+		  sqlite3_sourceid ());
+	  printf ("onion version %s\n", onion_version ());
 	  exit (EXIT_SUCCESS);
 	  break;
-	case 'H':
+	case 'H':		// --host <webhost>
 	  web_host = optarg;
 	  break;
-	case 'B':
+	case 'B':		// --sqlite-base <filepath>
 	  my_sqlite_path = optarg;
 	  break;
+	case 'C':		// --cpu-limit <cpuseconds>
+	  my_cputime_limit = atoi (optarg);
+	  if (my_cputime_limit < 0)
+	    FATAL ("invalid CPU time limit %d", my_cputime_limit);
+	  break;
+	case 'F':
+	  my_filesize_limit_mb = atoi (optarg);
+	  if (my_filesize_limit_mb < 0)
+	    FATAL ("invalid file size limit %d", my_filesize_limit_mb);
 	default:
 	  break;
 	}
@@ -167,7 +190,37 @@ show_usage (void)
 	  web_host);
   printf ("\t --sqlite-base | -B <path>    # set Sqlite file, default %s\n",
 	  my_sqlite_path);
+  printf
+    ("\t --cpu-limit | -C <cpusec>    # set CPU time limit, default %d sec,\n"
+     "\t                              # 0 for none.\n", my_cputime_limit);
+  printf
+    ("\t --file-limit | -F <sizemb>   # set file size limit, default %d megabytes\n"
+     "\t                              # 0 for none.\n", my_filesize_limit_mb);
 }				/* end show_usage */
+
+void
+set_process_limits (void)
+{
+  struct rlimit rlim;
+  memset (&rlim, 0, sizeof (rlim));
+  if (my_cputime_limit > 0)
+    {
+      rlim.rlim_cur = my_cputime_limit;
+      rlim.rlim_max = my_cputime_limit + 2;
+      if (setrlimit (RLIMIT_CPU, &rlim))
+	FATAL ("failed to limit CPU time to %d sec. (%s)", my_cputime_limit,
+	       strerror (errno));
+    };
+  memset (&rlim, 0, sizeof (rlim));
+  if (my_filesize_limit_mb > 0)
+    {
+      rlim.rlim_cur = my_filesize_limit_mb << 20;
+      rlim.rlim_max = my_filesize_limit_mb << 20 + 65536;
+      if (setrlimit (RLIMIT_FSIZE, &rlim))
+	FATAL ("failed to limit file size to %d megabytes (%s)",
+	       my_cputime_limit, strerror (errno));
+    };
+}				/* end set_process_limits */
 
 int
 main (int argc, char **argv)
@@ -177,22 +230,30 @@ main (int argc, char **argv)
 	   LOG_LOCAL0);
   parse_options (argc, argv);
   nice (5);
+  set_process_limits ();
   my_onion = onion_new (O_THREADED);
   errno = 0;
   {
     int sqin = sqlite3_initialize ();
     if (sqin != SQLITE_OK)
       FATAL ("Failed to initialize sqlite3: sqlite error %d (errno %s)", sqin,
-	     strerror(errno));
-    int sqop = sqlite3_open_v2(my_sqlite_path, &my_sqlite_db,
-			       SQLITE_OPEN_CREATE|SQLITE_OPEN_READWRITE|SQLITE_OPEN_FULLMUTEX,
-			       NULL);
+	     strerror (errno));
+    int sqop = sqlite3_open_v2 (my_sqlite_path, &my_sqlite_db,
+				SQLITE_OPEN_CREATE | SQLITE_OPEN_READWRITE |
+				SQLITE_OPEN_FULLMUTEX,
+				NULL);
     if (sqop != SQLITE_OK || !my_sqlite_db)
-      FATAL("Failed to open sqlite database %s, sqlite error %d (errno %s)",
-	    my_sqlite_path, sqop, strerror(errno));
+      FATAL ("Failed to open sqlite database %s, sqlite error %d (errno %s)",
+	     my_sqlite_path, sqop, strerror (errno));
   }
   onion_set_port (my_onion, web_port);
   onion_set_hostname (my_onion, web_host);
+  {
+    int sqcl = sqlite3_close_v2 (my_sqlite_db);
+    if (sqcl != 0)
+      FATAL ("Failed to close sqlite database %s, sqlite error %d (errno %s)",
+	     my_sqlite_path, sqcl, strerror (errno));
+  }
 }				/* end main */
 
 /****************
