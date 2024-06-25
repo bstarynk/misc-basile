cadf24f26f30da49cfa5e3afbfacb971f69d3e9a
Basile Starynkevitch <basile@starynkevitch.net>
2024-06-25 11:20:16 +0200

    adding --version processing to minicomp-gccjit
    Signed-off-by: Basile Starynkevitch <basile@starynkevitch.net>

--- minicomp-gccjit.c
+++ minicomp-gccjit.c
@@ -17,6 +17,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "libgccjit.h"
 
 
@@ -28,6 +29,14 @@ gcc_jit_context* jitctx;
 int
 main(int argc, char**argv)
 {
+  if (argc > 1 && !strcmp(argv[1], "--version")) {
+    printf("%s version: gitid %s\n using libgccjit %d.%d.%d\n built %s\n",
+	   argv[0], minicomp_gitid,
+	   gcc_jit_version_major(),
+	   gcc_jit_version_minor(),
+	   gcc_jit_version_patchlevel(), __DATE__ "@" __TIME__);
+    return 0;
+  };
   jitctx = gcc_jit_context_acquire();
   gcc_jit_context_release (jitctx);
   jitctx = NULL;
