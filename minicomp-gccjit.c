faeb1e54409753b3e1789f2577d5b1957fc64991
Basile Starynkevitch <basile@starynkevitch.net>
2024-06-28 21:09:31 +0200

    added minicomp_field_of_json to handle both union and struct
    Signed-off-by: Basile Starynkevitch <basile@starynkevitch.net>

--- minicomp-gccjit.c
+++ minicomp-gccjit.c
@@ -177,6 +177,30 @@ minicomp_jitloc (json_t *jloc)
 
 gcc_jit_type *minicomp_type_of_json (json_t * jtype, int rk);
 
+gcc_jit_field *
+minicomp_field_of_json (json_t *jcurfield, int rk, int flix)
+{
+  if (!json_is_object (jcurfield))
+    MINICOMP_FATAL ("minicomp_field_of_json rk=%d field#%d is bad %s",
+		    rk, flix,
+		    json_dumps (jcurfield, JSON_INDENT (1) | JSON_SORT_KEYS));
+  json_t *jfldname = json_object_get (jcurfield, "field-name");
+  json_t *jfldtype = json_object_get (jcurfield, "field-type");
+  if (!json_is_string (jfldname))
+    MINICOMP_FATAL
+      ("minicomp_field_of_json rk=%d bad field-name for field#%d is bad %s",
+       rk, flix, json_dumps (jcurfield, JSON_INDENT (1) | JSON_SORT_KEYS));
+  gcc_jit_type *fieldtype = minicomp_type_of_json (jfldtype, rk);
+  if (!fieldtype)
+    MINICOMP_FATAL
+      ("minicomp_field_of_json rk=%d bad field-type for field#%d is bad %s",
+       rk, flix, json_dumps (jcurfield, JSON_INDENT (1) | JSON_SORT_KEYS));
+  return gcc_jit_context_new_field (minicomp_jitctx,
+				    minicomp_jitloc (jcurfield), fieldtype,
+				    json_string_value (jfldname));
+}				/* end minicomp_field_of_json */
+
+
 gcc_jit_type *
 minicomp_add_type (json_t *jtype, const char *kind, int rk)
 {
@@ -217,7 +241,44 @@ minicomp_add_type (json_t *jtype, const char *kind, int rk)
 	MINICOMP_FATAL
 	  ("minicomp_add_type rk=%d out of memory for %d fields (%s)", rk,
 	   nbfields, strerror (errno));
-#warning incomplete minicomp_add_type struct
+      for (int flix = 0; flix < nbfields; flix++)
+	{
+	  json_t *jcurfield = json_array_get (jfields, flix);
+	  fldarr[flix] = minicomp_field_of_json (jcurfield, rk, flix);
+	};
+      gcc_jit_struct *jitstruct =
+	gcc_jit_context_new_struct_type (minicomp_jitctx,
+					 minicomp_jitloc (jtype),
+					 ctypename, nbfields, fldarr);
+      gcc_jit_type *jitopaqtyp = gcc_jit_struct_as_type (jitstruct);
+      free (fldarr);
+      return jitopaqtyp;
+    }
+  else if (!strcmp (kind, "union"))
+    {
+      json_t *jfields = json_object_get (jtype, "fields");
+      if (!json_is_array (jfields))
+	MINICOMP_FATAL ("minicomp_add_type rk=%d missing fields in jtype=%s",
+			rk,
+			json_dumps (jtype, JSON_INDENT (1) | JSON_SORT_KEYS));
+      int nbfields = json_array_size (jfields);
+      gcc_jit_field **fldarr =
+	calloc (((nbfields + 1) | 7), sizeof (gcc_jit_field *));
+      if (!fldarr)
+	MINICOMP_FATAL
+	  ("minicomp_add_type rk=%d out of memory for %d fields (%s)", rk,
+	   nbfields, strerror (errno));
+      for (int flix = 0; flix < nbfields; flix++)
+	{
+	  json_t *jcurfield = json_array_get (jfields, flix);
+	  fldarr[flix] = minicomp_field_of_json (jcurfield, rk, flix);
+	};
+      gcc_jit_type *jitunion =
+	gcc_jit_context_new_union_type (minicomp_jitctx,
+					minicomp_jitloc (jtype),
+					ctypename, nbfields, fldarr);
+      free (fldarr);
+      return jitunion;
     }
 #warning incomplete minicomp_add_type
   MINICOMP_FATAL ("incomplete minicomp_add_type rk=%d jtype=%s",
@@ -228,7 +289,6 @@ gcc_jit_type *
 minicomp_type_by_name (const char *tyname)
 {
   void *ptr = NULL;
-  gcc_jit_type *res = NULL;
   if (false)
     return NULL;
   else if (!strcmp (tyname, "void"))
@@ -326,6 +386,9 @@ minicomp_type_of_json (json_t *jtype, int rk)
 	MINICOMP_FATAL ("no jtype named %s rk#%d", json_string_value (jtype),
 			rk);
       return ty;
+    }
+  else if (json_is_object (jtype))
+    {
     }
   MINICOMP_FATAL ("incomplete minicomp_type_of_json rk=%d jtype=%s",
 		  rk, json_dumps (jtype, JSON_INDENT (1) | JSON_SORT_KEYS));
