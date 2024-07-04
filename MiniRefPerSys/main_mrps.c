67bca06b53b0d6a4f5030968f2c45b656ef61492
Basile Starynkevitch <basile@starynkevitch.net>
2024-07-04 18:59:08 +0200

    maybe starting MiniRefPerSys
    Signed-off-by: Basile Starynkevitch <basile@starynkevitch.net>

---
+++ MiniRefPerSys/main_mrps.c
@@ -0,0 +1,18 @@
+// file main_mrps.c
+//  SPDX-License-Identifier: GPL-3.0-or-later
+// 
+
+extern const char mrps_main_gitid[];
+const char mrps_main_gitid[]= MRPS_GITID;
+
+extern const char mrps_main_date[];
+const char mrps_main_date[]= __DATE__;
+
+extern const char mrps_main_shortgitid[];
+const char mrps_main_shortgitid[]= MRPS_SHORTGITID;
+
+#include "minrefpersys.h"
+
+int main(int argc, char**argv)
+{
+}
---
+++ MiniRefPerSys/minrefpersys.h
@@ -0,0 +1,7 @@
+/// file minrefpersys.h
+#ifndef MINREFPERSYS_INCLUDED
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+
+#endif /*MINREFPERSYS_INCLUDED*/
