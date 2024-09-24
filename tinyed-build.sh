904c26002304295d3cd81f3a57b3355465c6f076
Basile Starynkevitch <basile@starynkevitch.net>
2024-09-24 10:51:02 +0200

    tinyed-build.sh for fox-tinyed.cc uses the installed libfox 1.6 and g++-14
    Signed-off-by: Basile Starynkevitch <basile@starynkevitch.net>

--- tinyed-build.sh
+++ tinyed-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 # file misc-basile/tinyed-build.sh
-CXX=g++-13
+CXX=g++-14
 #ASTYLE=astyle
 #ASTYLEFLAGS='--verbose --indent=spaces=2  --style=gnu'
 #$ASTYLE $ASTYLEFLAGS fox-tinyed.cc
-PACKAGES="fox17 jsoncpp"
+PACKAGES="fox jsoncpp"
 GIT_ID=$(git log --format=oneline -q -1 | cut -c1-14)
 CXXFLAGS='-Wall  -Wextra -Woverloaded-virtual -Wshadow -O -g'
 $CXX $CXXFLAGS -fPIC -fPIE -DGIT_ID=\"$GIT_ID\" $(pkg-config --cflags $PACKAGES) fox-tinyed.cc $(pkg-config --libs $PACKAGES) -o fox-tinyed
