7bb2145a606f2f19ce5babdc05c89346d10a7943
Basile Starynkevitch <basile@starynkevitch.net>
2025-03-01 11:33:55 +0100

    use FOX-toolkit 1.7
    Signed-off-by: Basile Starynkevitch <basile@starynkevitch.net>

--- tinyed-build.sh
+++ tinyed-build.sh
@@ -4,7 +4,7 @@ CXX=g++-14
 #ASTYLE=astyle
 #ASTYLEFLAGS='--verbose --indent=spaces=2  --style=gnu'
 #$ASTYLE $ASTYLEFLAGS fox-tinyed.cc
-PACKAGES="fox jsoncpp"
+PACKAGES="fox17 jsoncpp"
 GIT_ID=$(git log --format=oneline -q -1 | cut -c1-14)
 CXXFLAGS='-Wall  -Wextra -Woverloaded-virtual -Wshadow -O -g'
 $CXX $CXXFLAGS -fPIC -fPIE -DGIT_ID=\"$GIT_ID\" $(pkg-config --cflags $PACKAGES) fox-tinyed.cc $(pkg-config --libs $PACKAGES) -o fox-tinyed
